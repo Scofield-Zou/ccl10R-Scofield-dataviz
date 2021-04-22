0aa7fda6cad0e51bf5c917b5cac650f19012bb0b
Scofield-Zou <71752886+Scofield-Zou@users.noreply.github.com>
2021-04-22 17:14:57 +0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,53 @@
+let data;
+
+function preload() {
+  data = loadJSON("./pose.json");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  console.log(data.bodypoints);
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  for (let point of data.bodypoints){
+    let x = point.x;
+    let y = point.y;
+    circle(x, y, 5);
+    textSize(5);
+    //text(point.part, x, y);
+  }
+
+  line(data.bodypoints[0].x,data.bodypoints[0].y,(data.bodypoints[5].x+data.bodypoints[6].x)/2,(data.bodypoints[5].y+data.bodypoints[6].y)/2);
+  fill(255);
+  //ellipse(data.bodypoints[3].x,data.bodypoints[3].y, 5, 10);
+  //ellipse(data.bodypoints[4].x,data.bodypoints[4].y, 5, 10);
+
+  // circle(297, 86, 42);
+
+  // circle(data.bodypoints[0].x,data.bodypoints[0].y, 7);
+  // circle(data.bodypoints[2].x,data.bodypoints[2].y, 8);
+  // circle(data.bodypoints[1].x,data.bodypoints[1].y, 8);
+
+  // fill(0);
+  // circle(data.bodypoints[1].x,data.bodypoints[1].y, 3);
+  // circle(data.bodypoints[2].x,data.bodypoints[2].y, 3);
+
+  line(data.bodypoints[5].x,data.bodypoints[5].y,data.bodypoints[6].x,data.bodypoints[6].y);
+  line(data.bodypoints[5].x,data.bodypoints[5].y,data.bodypoints[7].x,data.bodypoints[7].y);
+  line(data.bodypoints[7].x,data.bodypoints[7].y,data.bodypoints[9].x,data.bodypoints[9].y);
+  line(data.bodypoints[6].x,data.bodypoints[6].y,data.bodypoints[8].x,data.bodypoints[8].y);
+  line(data.bodypoints[8].x,data.bodypoints[8].y,data.bodypoints[10].x,data.bodypoints[10].y);
+
+  line(data.bodypoints[5].x,data.bodypoints[5].y,data.bodypoints[11].x,data.bodypoints[11].y);
+  line(data.bodypoints[6].x,data.bodypoints[6].y,data.bodypoints[12].x,data.bodypoints[12].y);
+  line(data.bodypoints[11].x,data.bodypoints[11].y,data.bodypoints[12].x,data.bodypoints[12].y);
+
+  line(data.bodypoints[11].x,data.bodypoints[11].y,data.bodypoints[13].x,data.bodypoints[13].y);
+  line(data.bodypoints[13].x,data.bodypoints[13].y,data.bodypoints[15].x,data.bodypoints[15].y);
+  line(data.bodypoints[12].x,data.bodypoints[12].y,data.bodypoints[14].x,data.bodypoints[14].y);
+  line(data.bodypoints[14].x,data.bodypoints[14].y,data.bodypoints[16].x,data.bodypoints[16].y);
+}
+
